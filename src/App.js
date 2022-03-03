54053a196db1a79193bfc1f00c45eff7f84c776d
Sharifa Mohamed <mhmdshari@gmail.com>
2022-03-03 16:43:25 +0530

    cleaned up

--- src/App.js
+++ src/App.js
@@ -6,14 +6,8 @@ import TinderCards from './TinderCards';
 function App() {
   return (
     <div className="app">
-    
-      {/* Header*/}
       <Header />
-
-      {/**  Tinder cardss*/}
       <TinderCards />
-
-      {/** Swipe Buttons */}
       <SwipeButtons />
     </div>
   );
--- src/axios.js
+++ src/axios.js
@@ -2,7 +2,6 @@ import axios from 'axios'
 
 const instance = axios.create({
   // baseURL: "http://localhost:8001"
-  //    baseURL:"https://sm-tinder-backend.herokuapp.com"
   baseURL: "https://tinderbackend--app.herokuapp.com/"
 
 });
